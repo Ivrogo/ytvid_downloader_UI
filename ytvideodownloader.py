b26e49c9f67c00b5e1f0abf5b6a45182afea7a56
Iván Rodríguez Gómez <er_jotx@hotmail.com>
2023-09-05 17:07:25 +0200

    added window, dependencies, youtubelogo, and some styles for the labels

--- ytvideodownloader.py
+++ ytvideodownloader.py
@@ -1,1 +1,29 @@
-print("hello world")
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from pytube import YouTube
+import threading
+
+
+# App frame
+app = tkinter.Tk()
+app.geometry("500x460")
+app.title("Youtube Downloader")
+app.resizable(height=False, width=False)
+canvas = Canvas(app, width=500, height=460)
+canvas.pack()
+
+
+# UI elements
+logo = PhotoImage(file="youtubelogo.png")
+logo = logo.subsample(10, 10)
+label = ttk.Style()
+label.configure("TLabel", foreground="#000000", font=("OCR A Extended", 14))
+
+
+# We add the elements to the canvas
+canvas.create_image(250, 80, image=logo)
+
+
+# Running the app
+app.mainloop()
